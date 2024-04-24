1d2783bb37720f841bafcfa3b0246c022b817b83
Daniel Henrique de Carvalho Teixeira Alves <105288296+danielcta@users.noreply.github.com>
2024-04-24 11:44:10 -0300

    Add files via upload

---
+++ C09EX11E.java
@@ -0,0 +1,14 @@
+public class C09EX11E
+{
+    public static void main(String[] args) 
+    {
+        double denominador = 1, somatorio = 0;
+
+        for (int i = 1; i <= 100; i++) 
+        {
+            System.out.println(somatorio);
+
+            
+        }
+    }
+}
